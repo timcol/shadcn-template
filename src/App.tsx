84e527ba47b3158cee73514d5b9a2f1ca4856fc1
Tim Collins <13359597+timcol@users.noreply.github.com>
2024-08-09 21:32:43 +1000

    initial commit with react and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from "@/components/ui/button"
+
+function App() {
+return (
+<div className="App">
+  <header className="App-header">
+    <Button>Click Me</Button>
+  </header>
+</div>
+);
+}
+
+export default App;
